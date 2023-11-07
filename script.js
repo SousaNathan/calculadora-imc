b224cfcf5bb96c49fc544bbeaa2957aef96d4c2a
Nathan Sousa <nathan.wks@outlook.com>
2023-11-06 23:28:48 -0300

    message error, calculo imc, resultado modal

---
+++ script.js
@@ -0,0 +1,27 @@
+const form = document.querySelector('form')
+const inputWeight = document.querySelector('#weight')
+const inputHeight = document.querySelector('#height')
+
+const modalWrapper = document.querySelector('.modal-wrapper')
+const modalMessage = document.querySelector('.modal .title span')
+const modalBtnClose = document.querySelector('.modal button.close')
+
+form.onsubmit = (event) => {
+  event.preventDefault()
+
+  const weight = inputWeight.value
+  const height = inputHeight.value
+  const result = IMC(weight, height)
+  const message = `Seu IMC é de ${result}`
+
+  modalMessage.innerText = message
+  modalWrapper.classList.add('open')
+}
+
+modalBtnClose.onclick = () => 
+  modalWrapper.classList.remove('open')
+
+
+IMC = (weight, height) => {
+  return (weight / ((height / 100) ** 2)).toFixed(2)
+}
