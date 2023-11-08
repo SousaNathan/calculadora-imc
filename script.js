ddd33811bba483b60479d2475b8c57f5e4dbc6ac
Nathan Sousa <nathan.wks@outlook.com>
2023-11-07 21:02:54 -0300

    refatorando js

--- script.js
+++ script.js
@@ -2,9 +2,18 @@ const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
 
-const modalWrapper = document.querySelector('.modal-wrapper')
-const modalMessage = document.querySelector('.modal .title span')
-const modalBtnClose = document.querySelector('.modal button.close')
+const Modal = {
+  wrapper: document.querySelector('.modal-wrapper'),
+  message: document.querySelector('.modal .title span'),
+  buttonClose: document.querySelector('.modal button.close'),
+
+  open() {
+    this.wrapper.classList.add('open')
+  },
+  close() {
+    this.wrapper.classList.remove('open')
+  }
+}
 
 form.onsubmit = (event) => {
   event.preventDefault()
@@ -14,13 +23,13 @@ form.onsubmit = (event) => {
   const result = IMC(weight, height)
   const message = `Seu IMC é de ${result}`
 
-  modalMessage.innerText = message
-  modalWrapper.classList.add('open')
+  Modal.message.innerText = message
+  Modal.open()
 }
 
-modalBtnClose.onclick = () => 
-  modalWrapper.classList.remove('open')
-
+Modal.buttonClose.onclick = () => {
+  Modal.close()
+}
 
 IMC = (weight, height) => {
   return (weight / ((height / 100) ** 2)).toFixed(2)
